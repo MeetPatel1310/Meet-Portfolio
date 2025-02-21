949b3cceb97262645e1acf6025f4c58563d6a7da
MeetPatel1310 <meetpatel2877@gmail.com>
2025-02-21 07:38:32 +0530

    Updated React Router settings and fixed GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Navbar from "./Components/Navbar/Navbar.jsx";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Experiance from "./Components/Experiance/Experiance.jsx";
@@ -15,19 +17,16 @@ function App() {
       
       
 
-      <Router>
-        <Routes>
-          {/* Define paths and associate components */}
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/experiance" element={<Experiance />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact/>} />
-
-          {/* Catch-all route for 404 pages */}
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </Router>
+      
+    <Router basename="/Meet-Portfolio">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/experiance" element={<Experiance />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
 
       
     </div>
