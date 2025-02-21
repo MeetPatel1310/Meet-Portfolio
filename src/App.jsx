391780b25b6c40dcc40ea875fdf235858f628f9b
MeetPatel1310 <meetpatel2877@gmail.com>
2025-02-21 07:50:59 +0530

    Fixed React Router issue for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
@@ -13,16 +14,16 @@ import Contact from "./Components/Contact/Contact.jsx";
 
 function App() {
   return (
-    <Router basename="/Meet-Portfolio">
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/experiance" element={<Experiance />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  </Router>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/experiance" element={<Experiance />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
